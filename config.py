4de9fdd299efbf3d765c3a329c919dabd4417a92
Nyasha "Grace <123832803+Gracicinya@users.noreply.github.com>
2026-04-08 15:48:40 -0400

    Assets

---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py - Configuration constants for the game
+
+# Screen settings
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Font settings
+STORY_FONT_SIZE = 26
+
+# Game title
+GAME_TITLE = "The Remaking of Lakechamp Town"
+
+# Font settings
+STORY_FONT_SIZE = 26
+
+# Game title
+GAME_TITLE = "The Remaking of Lakechamp Town"
+
+# Mini-game mappings
+MINI_GAMES = {
+    "guess_pin": "guess_pin",
+    "puzzle_map": "puzzle_map",
+    "slot_placement": "slot_placement",
+    "road_crossing": "road_crossing",
+}
